87d22e9ec7553780877e0f7db8d007cd38999155
bwengye <61537375+bwengye@users.noreply.github.com>
2021-03-31 09:01:40 +0300

    Add files via upload

---
+++ constants.h
@@ -0,0 +1,11 @@
+// Define pins for ultrasonic sensors and pumps
+const int trigPin1 = 10;
+const int echoPin1 = 9;
+const int trigPin2 = 12;
+const int echoPin2 = 11;
+// An easier way of defining our constants is to use the #define macro . 
+//Note that this does not need termination and equal signs for pin assignments or declaration. 
+#define pump1 5
+#define pump2 6  
+// same as
+//const int pump1=5;
